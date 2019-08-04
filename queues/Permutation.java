81b19ee520e6559921a4ecda804ba95bbe8849c9
wangyixu <cauwangyixu@126.com>
2019-08-04 22:55:05 +0800

    update

--- queues/Permutation.java
+++ queues/Permutation.java
@@ -15,15 +15,6 @@ import edu.princeton.cs.algs4.StdOut;
      Timing:       193/193 tests passed
 
      Aggregate score: 99.90%
-
-     Memory:
-        Test 3 (bonus): check that maximum size of any or Deque or RandomizedQueue object
-            created is equal to k
-        Test 7: Worst-case constant memory allocated or deallocated per deque operation?
-         - when current size of Deque was 16 objects;
-         - the call to addFirst()
-         - caused a change in memory of 128 bytes
-         - any change of more than 96 bytes fails the test
  *
  * Created by yixu on 2017/1/7, edited on 2018/8/28.
  */
